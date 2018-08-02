bc22b2e256831a7bff96dcb5f74148bb53960be8
Bobby de Vos <bobby_devos@sil.org>
2018-08-02 12:02:29 -0600

    Position sub sub forms better

--- tools/position_subs.py
+++ tools/position_subs.py
@@ -8,26 +8,20 @@ ufo = sys.argv[1]
 font = OpenFont(ufo)
 
 # Modify UFO
-
-# Make sure we only have one layer
-# for layer in font.layers:
-#     print(layer.name)
-
-# Show the default layer
-# print(font.defaultLayer.name)
-
 layer = font.getLayer(font.defaultLayer.name)
 for glyph in layer:
     for anchor in glyph.anchors:
-        # print('{} {} x:{} y:{}'.format(glyph.name, anchor.name, anchor.x, anchor.y))
+        # print(f'{glyph.name} {anchor.name} x:{anchor.x} y:{anchor.y}')
+        (xmin, ymin, xmax, ymax) = glyph.bounds
         if anchor.name == '_Sub':
             # Mark
-            anchor.x = -600
-            anchor.y = -300
+            anchor.x = xmin
+            anchor.y = 0
         if anchor.name == 'Sub':
             # Base
-            anchor.x = 600
-            anchor.y = -600
+            # print(f'{glyph.name}: {xmin} {xmax}')
+            anchor.x = xmax + 50
+            anchor.y = -200
 
 # Save UFO
 font.changed()
