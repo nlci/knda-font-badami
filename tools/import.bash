3a4203ecd07acd5b1b40bcbf2b149d73701f1c68
Bobby de Vos <bobby_devos@sil.org>
2019-01-11 15:33:56 -0700

    Slightly expand and improve the character set

--- tools/import.bash
+++ tools/import.bash
@@ -4,8 +4,16 @@ face="$1"
 style="$2"
 ufo="$3"
 
+bengf="Bhagirati"
 devaf="Maurya"
 tamlf="Vaigai"
 
+if [ "$style" = "BoldItalic" ]
+then
+    styleb="Bold"
+else
+    styleb="$style"
+fi
+psfcopyglyphs -f --rename rename --unicode usv -i ../cs/bhagirati/main4knda.csv -s "${beng}/${bengf}-${styleb}.ufo" ${ufo}
 psfcopyglyphs -f --rename rename --unicode usv -i ../cs/panini/main4knda.csv -s "${deva}/${devaf}-${style}.ufo" ${ufo}
 psfcopyglyphs -f --rename rename --unicode usv -i ../cs/thiruvalluvar/main.csv -s "${taml}/${tamlf}-${style}.ufo" ${ufo}
