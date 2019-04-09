3d5be3bf67f3d8be382eef2d270b536bc1228eb9
Bobby de Vos <bobby_devos@sil.org>
2019-04-09 12:56:00 -0600

    Import Latin glyphs from UFO, not TFF, sources

--- addchars.py
+++ addchars.py
@@ -1,49 +1,33 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 from addcharslib import *
 
-def modifySource(sfd, f, s, sn):
-    print(sfd)
+for f in faces:
 
     workshop = 1.4
     upm2048 = 1000.0/2048.0
     upm1000 = 1.0
-    scale2048 = '-s ' + str(upm2048/workshop) + ' '
-    scale1000 = '-s ' + str(upm1000/workshop) + ' '
+    scale2048 = str(upm2048/workshop)
+    scale1000 = str(upm1000/workshop)
 
-    asn = sn
-    asn = asn.replace('BoldItalic', 'Bold')
-    asn = asn.replace('Italic', 'Regular')
-    cmd = scale2048 + '-i ' + annapurna + asn + '.ttf' + ' --rangefile cs/annapurna/main.txt'
-    modifyFile(cmd, sfd)
+    lighter = {
+        'Regular': 'Medium',
+        'Italic': 'MediumItalic',
+        'Bold': 'Demibold',
+        'Bold Italic': 'DemiboldItalic'
+        }
+    heavier = {
+        'Regular': 'Demibold',
+        'Italic': 'DemiboldItalic',
+        'Bold': 'Bold',
+        'Bold Italic': 'BoldItalic'
+        }
 
-    if f == 'Kaveri':
-        lighter = {
-            'Regular': 'Medium',
-            'Italic': 'MediumItalic',
-            'Bold': 'SemiBold',
-            'BoldItalic': 'SemiBoldItalic'
-            }
-        heavier = {
-            'Regular': 'SemiBold',
-            'Italic': 'SemiBoldItalic',
-            'Bold': 'Bold',
-            'BoldItalic': 'BoldItalic'
-            }
-        esn = lighter[sn]
-        esn = heavier[sn]
-        cmd = scale1000 + '-i ' + exo + esn + '.ttf' + ' --namefile cs/exo/main_glyphs.txt --rangefile cs/exo/pre.txt --rangefile cs/exo/main.txt'
-        modifyFile(cmd, sfd)
-        cmd = scale2048 + '-i ' + charis + s + '.ttf' + ' --rangefile cs/charis/composite4gentium.txt --rangefile cs/charis/extra4exo.txt'
-        modifyFile(cmd, sfd)
-    else:
-        gs = s.replace('-', '')
-        cmd = scale2048 + '-i ' + gentium + gs + '.ttf' + ' --namefile cs/gentium/main_glyphs.txt --rangefile cs/gentium/pre.txt --rangefile cs/gentium/main.txt'
-        modifyFile(cmd, sfd)
-        cmd = scale2048 + '-i ' + charis + s + '.ttf' + ' --rangefile cs/charis/composite4gentium.txt --rangefile cs/charis/extra4gentium.txt'
-        modifyFile(cmd, sfd)
-
-for f in faces:
-    for (s, sn) in zip(styles, stylesName):
-        sn = sn.replace(' ', '')
-        modifySource(f + '-' + sn + '.sfd', f, s, sn)
+    for sn in stylesName:
+        if f == 'Kaveri':
+            # esn = lighter[sn]
+            esn = heavier[sn]
+            modifyFile(scale1000, 'exo', f, sn, esn)
+            modifyFile(scale2048, 'charis', f, sn)
+        else:
+            modifyFile(scale2048, 'gentium', f, sn)
