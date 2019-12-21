abe9e6f8317d28d75450ce63cf50bb0784e30bc9
Bobby de Vos <bobby_devos@sil.org>
2019-12-20 17:08:32 -0700

    import SIDDHAM from a different font

--- tools/import.bash
+++ tools/import.bash
@@ -4,16 +4,16 @@ face="$1"
 style="$2"
 ufo="$3"
 
-bengf="Bhagirati"
+oryaf="Asika"
 devaf="Maurya"
 tamlf="Vaigai"
 
 if [ "$style" = "BoldItalic" ]
 then
-    styleb="Bold"
+    styleo="Bold"
 else
-    styleb="$style"
+    styleo="$style"
 fi
-psfcopyglyphs -f --rename rename --unicode usv -i ../cs/bhagirati/main4knda.csv -s "${beng}/${bengf}-${styleb}.ufo" ${ufo}
+psfcopyglyphs -f --rename rename --unicode usv -i ../cs/asika/main4knda.csv -s "${orya}/${oryaf}-${styleo}.ufo" ${ufo}
 psfcopyglyphs -f --rename rename --unicode usv -i ../cs/panini/main4knda.csv -s "${deva}/${devaf}-${style}.ufo" ${ufo}
 psfcopyglyphs -f --rename rename --unicode usv -i ../cs/thiruvalluvar/main.csv -s "${taml}/${tamlf}-${style}.ufo" ${ufo}
