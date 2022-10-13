907235dc2c435807996161fca41cc7fbc93341c3
Bobby de Vos <bobby_devos@sil.org>
2022-10-13 06:29:24 -0600

    Improve some glyphs

--- tools/cleanup.py
+++ tools/cleanup.py
@@ -8,8 +8,17 @@ ufo = sys.argv[1]
 font = OpenFont(ufo)
 
 # Modify UFO
+
+# we don't want to see the dottedcircle
+# to work around an issue with shaping engines
 dottedcircle = font['dottedcircle']
 dottedcircle.width = 0
+
+# candrabindu is a zero width mark
+candrabindu = font['candrabindu']
+width = candrabindu.width
+candrabindu.leftMargin = -width
+candrabindu.width = 0
 
 # Save UFO
 font.changed()
--- tools/encoding.py
+++ tools/encoding.py
@@ -9,7 +9,20 @@ font = OpenFont(ufo)
 
 # Modify UFO
 
+# ssa.sub RSB way too much to the left in Badami
+if font.info.familyName == 'Badami':
+    source = font['pha.sub']
+    target = font['ssa.sub']
+    target.rightMargin = source.rightMargin
+
+# make candrabindu from the om sign
+spacingcandrabindu = font['spacingcandrabindu']
+spacingcandrabindu.decompose()
+for contour in spacingcandrabindu.contours:
+    if len(contour) > 10:
+        spacingcandrabindu.removeContour(contour)
+
 # Save UFO
-# font.changed()
-# font.save()
-# font.close()
+font.changed()
+font.save()
+font.close()
